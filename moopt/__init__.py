0b7c3d141f04cc5fa3acbc23a54f6afde509c16d
marcosmrai <marcosmrai@gmail.com>
2022-10-29 17:59:44 -0300

    Update __init__.py

--- moopt/__init__.py
+++ moopt/__init__.py
@@ -3,7 +3,7 @@ from .nc import nc
 from .monise import monise
 from .nise import nise
 from .pgen import pgen
-from .rennen import rennen
+#from .rennen import rennen
 from .random_weights import random_weights
-from .xnise import xnise
+#from .xnise import xnise
 from .esse import esse
